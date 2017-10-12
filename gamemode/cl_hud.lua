78d4474aeb14321c6b7b57666694bb191eeb8608
aSharpDev <sharpdeveloper@hotmail.com>
2017-10-12 00:08:39 -0400

    Add files via upload

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -1,3 +1,7 @@
+/*
+	Hide the default HUD
+*/
+
 local elements = {
 	["CHudHealth"] 				= true,
 	["CHudBattery"] 			= true,
@@ -9,6 +13,10 @@ local function HideDefaultHUD(name)
 	if (elements[name]) then return false end
 end
 hook.Add("HUDShouldDraw", "HideDefaultHUD", HideDefaultHUD)
+
+/*
+	Create some fonts
+*/
 
 surface.CreateFont("AliveCount", {
 	font = "DermaLarge",
@@ -28,9 +36,13 @@ surface.CreateFont("PrimaryAmmoReserve", {
 	weight = 500
 })
 
+/*
+	Display the time left for preperation
+*/
+
 local TimeLeft = 0
-net.Receive("DisplayTimer", function()
-  TimeLeft = net.ReadInt(32)
+net.Receive("BroadcastTimer", function()
+	TimeLeft = net.ReadInt(32)
 end)
 
 local function HUD_PrepTime()
@@ -45,6 +57,31 @@ local function HUD_PrepTime()
   end
 end
 
+/*
+	Display the winner of the round
+*/
+
+local Winner = nil
+net.Receive("BroadcastWinner", function()
+	Winner = net.ReadString()
+end)
+
+local function HUD_DisplayWinner()
+	if (Winner != nil) then
+		surface.SetFont("DermaLarge")
+		local text = string.upper(Winner) .. " HAS WON THE ROUND"
+		local w, h = surface.GetTextSize(text)
+		local width, height = w + 20, h + 10
+
+		draw.RoundedBox(0, (ScrW() / 2) - (width / 2), (ScrH() * 0.1) - (height / 2), width, height, Color(255, 255, 255, 150))
+    draw.SimpleText(text, "DermaLarge", ScrW() / 2, ScrH() * 0.1, Color(0, 0, 0, 200), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+	end
+end
+
+/*
+	Display how many players are alive
+*/
+
 local function HUD_AliveCount()
   local w, h = ScrW() * 0.125, ScrH() * 0.06
   local padding = 25
@@ -55,19 +92,19 @@ local function HUD_AliveCount()
   draw.SimpleText("ALIVE", "AliveCount", ScrW() - (((w - h) / 2) + padding), (h / 2) + padding, Color(0, 0, 0, 200), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
 end
 
+/*
+	Display Health, Armor, Ammo, etc.
+*/
+
 local BulletMat = Material("vgui/flaticons/bulletAmmo.png")
 local function HUD_ClientStats()
 	local client = LocalPlayer()
-
 	if (!client:Alive()) then return end
 
 	local w, h = ScrW() * 0.25, ScrH() * 0.04
 	local padding = 25
 
-	/*
-		Health
-	*/
-
+	// Health
 	local HealthColor = color_white
 
 	if (client:Health() < 50) then
@@ -82,24 +119,24 @@ local function HUD_ClientStats()
 
 	draw.RoundedBox(0, (ScrW() / 2) - (w / 2), ScrH() - (h + padding), w * (client:Health() / client:GetMaxHealth()), h, HealthColor)
 
-	/*
-		Ammo
-	*/
-
+	// Ammo
 	local wep = client:GetActiveWeapon()
-
 	if (!IsValid(wep)) then return end
 
 	if (wep:Clip1() != -1) then
 		draw.SimpleText(wep:Clip1(), "PrimaryAmmo", (ScrW() / 2) - 15, ScrH() - ((h + padding) + 5), color_white, TEXT_ALIGN_RIGHT, TEXT_ALIGN_BOTTOM)
-		local size = ScreenScale(15)
 
+		local size = ScreenScale(15)
 		surface.SetDrawColor(color_white)
 		surface.SetMaterial(BulletMat)
  		surface.DrawTexturedRect((ScrW() / 2) - (size / 2), ScrH() - ((h + padding + 5) + size), size, size)
 		draw.SimpleText(client:GetAmmoCount(wep:GetPrimaryAmmoType()), "PrimaryAmmoReserve", (ScrW() / 2) + 15, ScrH() - ((h + padding) + 5), color_white, TEXT_ALIGN_LEFT, TEXT_ALIGN_BOTTOM)
 	end
 end
+
+/*
+	Display the Radar
+*/
 
 local function HUD_RadarCamera()
 	local w, h = ScrW() * 0.15, ScrW() * 0.15
@@ -126,4 +163,5 @@ function GM:HUDPaint()
   HUD_AliveCount()
 	HUD_ClientStats()
 	HUD_RadarCamera()
+	HUD_DisplayWinner()
 end
--- gamemode/sv_roundsystem.lua
+++ gamemode/sv_roundsystem.lua
@@ -13,15 +13,18 @@ function GBRounds.Start()
 
   for k, v in pairs(player.GetAll()) do
     v:Spawn()
-    v:StripWeapons()
   end
 
   print("Round started!")
 end
 
+util.AddNetworkString("BroadcastWinner")
 function GBRounds.End(winner)
   GBRounds.TimeLeft = GBRounds.PrepTime
-  GBRounds.RoundActive = false
+
+  net.Start("BroadcastWinner")
+    net.WriteString(tostring(winner:Nick()))
+  net.Broadcast()
 
   print(winner:Nick() .. " has won the game!")
   print("Round ended! Next round begins in " .. GBRounds.PrepTime .. " seconds.")
@@ -32,21 +35,23 @@ function GBRounds.Think()
     for k, v in pairs(player.GetAll()) do
       if (player.GetAllAlive() < 2 and v:Alive()) then
         GBRounds.End(v)
+        timer.Simple(10, function()
+          GBRounds.RoundActive = false
+        end)
       end
     end
   end
 end
 hook.Add("Think", "GBRounds.Think", GBRounds.Think)
 
-util.AddNetworkString("DisplayTimer")
+util.AddNetworkString("BroadcastTimer")
 function GBRounds.Logic()
   if (player.GetAllAlive() > 1 and !GBRounds.RoundActive) then
     print(GBRounds.TimeLeft)
-    for k, v in pairs(player.GetAll()) do
-      net.Start("DisplayTimer")
-        net.WriteInt(GBRounds.TimeLeft, 32)
-      net.Send(v)
-    end
+
+    net.Start("BroadcastTimer")
+      net.WriteInt(GBRounds.TimeLeft, 32)
+    net.Broadcast()
 
     if (GBRounds.TimeLeft <= 0) then
       GBRounds.Start()
